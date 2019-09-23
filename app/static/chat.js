230f95efcca793e3b0a044bdb8cbf144e086651a
Ulmer Varly <uvarly@vo-k5.21-school.ru>
2019-09-23 15:50:37 +0300

    minor changes

--- app/static/chat.js
+++ app/static/chat.js
@@ -21,7 +21,7 @@ function send_message(you_id) {
     request.onreadystatechange = function () {
         if (request.status === 201 && request.readyState === 4) {
             let resp = JSON.parse(request.responseText);
-            generate_your_message({
+            generate_my_message({
                 'sender': resp['id'],
                 'receiver': you_id,
                 'message_date': resp['message_date'],
@@ -50,7 +50,7 @@ function generate_your_message(json_message) {
     let message_time = document.createElement('span');
     message_time.className = 'message-time';
     message_time.innerText = json_message['message_date'];
-    let small = document.createElement('p');
+    let small = document.createElement('small');
     small.className = 'pull-right text-muted';
     small.appendChild(glyph);
     small.appendChild(message_time);
@@ -63,6 +63,47 @@ function generate_your_message(json_message) {
     inner_div.appendChild(small);
     let message = document.createElement('p');
     message.innerText = json_message['text'];
+    let outer_div = document.createElement('div');
+    outer_div.className = 'chat-body clearfix';
+    outer_div.appendChild(inner_div);
+    outer_div.appendChild(message);
+
+    element.appendChild(outer_div);
+    document.getElementById('chat').appendChild(element);
+}
+
+function generate_my_message(json_message) {
+    let element = document.createElement('li');
+    element.className = 'right clearfix';
+    let span = document.createElement('span');
+    let img = document.createElement('img');
+    span.className = 'chat-img pull-right';
+    img.src = document.getElementById(json_message['sender']).src;
+    img.className = 'img-circle';
+    img.style.width = '64px';
+    img.style.height = '64px';
+    span.appendChild(img);
+    element.appendChild(span);
+
+    let glyph = document.createElement('span');
+    glyph.className = 'glyphicon glyphicon-time';
+    let message_time = document.createElement('span');
+    message_time.className = 'message-time';
+    message_time.innerText = json_message['message_date'];
+    let small = document.createElement('small');
+    small.className = ' text-muted';
+    small.appendChild(glyph);
+    small.appendChild(message_time);
+    let strong = document.createElement('strong');
+    strong.className = 'pull-right primary-front';
+    strong.innerText = document.getElementById(json_message['sender']).className;
+    let inner_div = document.createElement('div');
+    inner_div.className = 'header';
+    inner_div.appendChild(small);
+    inner_div.appendChild(strong);
+    let message = document.createElement('p');
+    message.innerText = json_message['text'];
+    message.className = 'message-right';
     let outer_div = document.createElement('div');
     outer_div.className = 'chat-body clearfix';
     outer_div.appendChild(inner_div);
